905aaa831953f50d7abae6276fddef39af4a2ab6
Felipe Sousa Developer <fesousalocutor@gmail.com>
2022-08-04 00:40:22 -0300

    config: conclude test husky and lint-staged

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ const port = process.env.PORT || 5001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => console.log(`🚀 API is running on port 👉 ${port}`));
